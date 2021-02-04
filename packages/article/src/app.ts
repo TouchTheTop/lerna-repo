24df85dc80f7cd15a6bb7e243372bff0c825e91e
christine <1204356932@qq.com>
2021-02-04 16:37:13 +0800

    test

--- packages/article/src/app.ts
+++ packages/article/src/app.ts
@@ -3,14 +3,10 @@ import VueRouter from 'vue-router'
 import Index from './Index';
 import {default as Article} from './components';
 
-
-import { GenRequest } from "core/util";
-
 import "../../../style/themes/index.less";
 
 export {
-    Article,
-    GenRequest
+    Article
 }
 export default Index
 
