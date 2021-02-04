29dc581992a48097693acb64aa06479442d2ab2b
christine <1204356932@qq.com>
2021-02-04 17:11:38 +0800

    test

--- packages/article/src/Index/Index.vue
+++ packages/article/src/Index/Index.vue
@@ -1,20 +1,17 @@
 <template>
   <div>
     <h1>-----【文章】-----</h1>
-    <loading></loading>
+
     <button @click="toDetail">点击跳转详情</button>
   </div>
 </template>
 <script>
 // 引入公共库中的组件
-import { Loading } from "core";
+
 import Api from "../../api";
 export default {
   name: "QAvatar",
   props: {},
-  components: {
-    Loading,
-  },
   data() {
     return {};
   },
