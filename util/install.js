abc7bb034678bffeb61fad2b2ad0bd92acf55f74
christine <1204356932@qq.com>
2021-02-19 14:09:22 +0800

    feat: husky

--- util/install.js
+++ util/install.js
@@ -1,22 +1,21 @@
 // component:入口组件 deps:依赖组件
 
-let a = 1;
-// export const withInstall = function(component, deps, routers) {
-//   const c = component;
-//   c.install = function(app) {
-//     app.component(component.name, component);
-//     deps &&
-//       deps.forEach((dep) => {
-//         app.component(dep.name, dep);
-//       });
-//   };
-//   return c;
-// };
+export const withInstall = function (component, deps, routers) {
+  const c = component;
+  c.install = function (app) {
+    app.component(component.name, component);
+    deps &&
+      deps.forEach((dep) => {
+        app.component(dep.name, dep);
+      });
+  };
+  return c;
+};
 
-// // 内部路由注册到vue实例
-// export const rejectRouter = function(routers) {
-//   const r = routers;
-//   return function(appCase) {
-//     appCase.$router.addRoutes(r);
-//   };
-// };
+// 内部路由注册到vue实例
+export const rejectRouter = function (routers) {
+  const r = routers;
+  return function (appCase) {
+    appCase.$router.addRoutes(r);
+  };
+};
