145d704b65572388f8e7a226b55686e71494366e
christine <1204356932@qq.com>
2021-02-19 10:59:40 +0800

    tt

--- util/install.js
+++ util/install.js
@@ -1,20 +1,22 @@
 // component:入口组件 deps:依赖组件
-export const withInstall = function(component, deps, routers) {
-  const c = component;
-  c.install = function(app) {
-    app.component(component.name, component);
-    deps &&
-      deps.forEach((dep) => {
-        app.component(dep.name, dep);
-      });
-  };
-  return c;
-};
 
-// 内部路由注册到vue实例
-export const rejectRouter = function(routers) {
-  const r = routers;
-  return function(appCase) {
-    appCase.$router.addRoutes(r);
-  };
-};
+let a = 1;
+// export const withInstall = function(component, deps, routers) {
+//   const c = component;
+//   c.install = function(app) {
+//     app.component(component.name, component);
+//     deps &&
+//       deps.forEach((dep) => {
+//         app.component(dep.name, dep);
+//       });
+//   };
+//   return c;
+// };
+
+// // 内部路由注册到vue实例
+// export const rejectRouter = function(routers) {
+//   const r = routers;
+//   return function(appCase) {
+//     appCase.$router.addRoutes(r);
+//   };
+// };
